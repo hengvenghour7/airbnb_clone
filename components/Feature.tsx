9836d7a57b1569fb15329f1f05853815d5c2624e
venghour <hengvenghour7@gmail.com>
2025-10-04 16:16:38 +1000

    fix imagepath error

--- components/Feature.tsx
+++ components/Feature.tsx
@@ -8,49 +8,49 @@ export default function Feature ({catalogue}: {catalogue:string}) {
     const allFeature = [
         {
             name: 'Opera House',
-            imageSrc: '/images/tourImg1.jpg',
+            imageSrc: './images/tourImg1.jpg',
             price: 200,
             content: 'Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica'
         },
         {
             name: 'Darling Habour',
-            imageSrc: '/images/tourImg2.jpg',
+            imageSrc: './images/tourImg2.jpg',
             price: 350,
             content: 'Opening to the azure vistas of the sea, The Darling Harbour is a pedestrian and a recreational quarter filled with exhibitions, museums, entertainment venues, shops and restaurants.'
         },
         {
             name: 'Hyde park',
-            imageSrc: '/images/tourImg3.jpg',
+            imageSrc: './images/tourImg3.jpg',
             price: 400,
             content: 'Opening to the azure vistas of the sea, The Darling Harbour is a pedestrian and a recreational quarter filled with exhibitions, museums, entertainment venues, shops and restaurants.'
         },
         {
             name: 'Darling Habour',
-            imageSrc: '/images/tourImg2.jpg',
+            imageSrc: './images/tourImg2.jpg',
             price: 380,
             content: 'Opening to the azure vistas of the sea, The Darling Harbour is a pedestrian and a recreational quarter filled with exhibitions, museums, entertainment venues, shops and restaurants.'
         },
         {
             name: 'Darling Habour',
-            imageSrc: '/images/tourImg2.jpg',
+            imageSrc: './images/tourImg2.jpg',
             price: 330,
             content: 'Opening to the azure vistas of the sea, The Darling Harbour is a pedestrian and a recreational quarter filled with exhibitions, museums, entertainment venues, shops and restaurants.'
         },
         {
             name: 'Darling Habour',
-            imageSrc: '/images/tourImg2.jpg',
+            imageSrc: './images/tourImg2.jpg',
             price: 330,
             content: 'Opening to the azure vistas of the sea, The Darling Harbour is a pedestrian and a recreational quarter filled with exhibitions, museums, entertainment venues, shops and restaurants.'
         },
         {
             name: 'Darling Habour',
-            imageSrc: '/images/tourImg2.jpg',
+            imageSrc: './images/tourImg2.jpg',
             price: 330,
             content: 'Opening to the azure vistas of the sea, The Darling Harbour is a pedestrian and a recreational quarter filled with exhibitions, museums, entertainment venues, shops and restaurants.'
         },
         {
             name: 'Darling Habour',
-            imageSrc: '/images/tourImg2.jpg',
+            imageSrc: './images/tourImg2.jpg',
             price: 330,
             content: 'Opening to the azure vistas of the sea, The Darling Harbour is a pedestrian and a recreational quarter filled with exhibitions, museums, entertainment venues, shops and restaurants.'
         },
