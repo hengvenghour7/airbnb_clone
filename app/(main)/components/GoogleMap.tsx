2c1af12cb29cd1bae6369f50600c430e17cbe3e5
venghour <hengvenghour7@gmail.com>
2025-11-02 02:24:15 +1100

    fix google api is already presented

--- app/(main)/components/GoogleMap.tsx
+++ app/(main)/components/GoogleMap.tsx
@@ -6,6 +6,15 @@ export default function MyMap() {
   const center = useMemo(() => ({ lat: -37.8136, lng: 144.9631 }), []); // Melbourne
 
   return (
+    (typeof window !== 'undefined' && window.google && window.google.maps) ? 
+      <GoogleMap
+        mapContainerStyle={{ width: '100%', height: '100%' }}
+        center={center}
+        zoom={12}
+      >
+        <Marker position={center} />
+      </GoogleMap>
+     :
     <LoadScript googleMapsApiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY!}>
       <GoogleMap
         mapContainerStyle={{ width: '100%', height: '100%' }}
